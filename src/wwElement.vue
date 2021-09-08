0d1a622250441c22791a80393df6243e680fb56d
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-09-08 16:30:12 +0200

    Try ww-drag-drop = false

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -2,7 +2,7 @@
     <div class="element-container" :style="cssVariables">
         <div class="swiper-container" :class="'swiper-free-mode ' + 'unique-swipper-container-' + uniqueID">
             <wwLayout
-                disable-drag-drop="true"
+                disable-drag-drop="false"
                 path="mainLayoutContent"
                 class="swiper-wrapper"
                 @update:list="handleUpdate($event)"
