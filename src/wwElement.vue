eef973ef5fb795caa06272029d47990597a9bb86
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-04-06 16:03:31 +0200

    update with force update

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="element-container" :style="cssVariables" :class="{ editing: isEditing, selected: isSelected }">
-        <div ref="swiper" :key="componentKey" class="swiper" ww-responsive="swiper">
+        <div ref="swiper" class="swiper" ww-responsive="swiper">
             <wwLayout
                 :disable-drag-drop="true"
                 path="mainLayoutContent"
@@ -73,7 +73,6 @@ export default {
     data() {
         return {
             sliderIndex: 0,
-            componentKey: 0,
         };
     },
     computed: {
@@ -208,7 +207,7 @@ export default {
         initSwiper(resetIndex = true) {
             if (!window.__WW_IS_PRERENDER__) {
                 if (this.swiperInstance && this.swiperInstance.destroy) this.swiperInstance.destroy(true, true);
-                this.componentKey += 1;
+                this.$forceUpdate();
                 if (!this.isValidContent) return;
                 this.$nextTick(() => {
                     this.swiperInstance = new Swiper(this.$refs.swiper, this.swiperOptions);
